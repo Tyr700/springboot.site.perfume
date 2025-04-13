581eed872b610df2ecd638ade19daef4a555069e
Tyr700 <tlolivira970@gmail.com>
2025-04-13 16:42:57 -0300

    Initial commit

---
+++ src/test/java/site/perfume/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package site.perfume.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
