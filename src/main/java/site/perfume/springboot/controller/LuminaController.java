9a0d64e8712a885fdb4cde3aaddcc355820aa5a1
Tyr700 <tlolivira970@gmail.com>
2025-04-19 14:17:49 -0300

    Feat: criado a base do controller de Lumina

---
+++ src/main/java/site/perfume/springboot/controller/LuminaController.java
@@ -0,0 +1,12 @@
+package site.perfume.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Lumina")
+public class LuminaController {
+
+
+}
