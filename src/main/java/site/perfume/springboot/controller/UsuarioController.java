c5456d95db878303393f2ed5d31fcca410d5c6c9
Tyr700 <tlolivira970@gmail.com>
2025-04-19 15:09:33 -0300

    Feat: criado o método post: criar usuário

--- src/main/java/site/perfume/springboot/controller/UsuarioController.java
+++ src/main/java/site/perfume/springboot/controller/UsuarioController.java
@@ -1,11 +1,38 @@
 package site.perfume.springboot.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.perfume.springboot.errors.UsuarioJaExiste;
+import site.perfume.springboot.model.Usuario;
+import site.perfume.springboot.service.UsuarioService;
+import java.util.List;
+
+
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<?> criarUsuario(@RequestBody Usuario usuario) {
+        try {
+            var user = usuarioService.criarUsuario(usuario);
+            return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        } catch (UsuarioJaExiste e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMensagem());
+        }
+    }
+
 
 }
