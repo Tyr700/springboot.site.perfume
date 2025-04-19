6f2d07e814a51e56d1a6c7e559195a7168ffe8c6
Tyr700 <tlolivira970@gmail.com>
2025-04-19 17:39:13 -0300

    Feat: adição de ILuminaRepository a classe LuminaService

--- src/main/java/site/perfume/springboot/service/LuminaService.java
+++ src/main/java/site/perfume/springboot/service/LuminaService.java
@@ -2,7 +2,16 @@ package site.perfume.springboot.service;
 
 
 import org.springframework.stereotype.Service;
+import site.perfume.springboot.repository.ILuminaRepository;
 
 @Service
 public class LuminaService {
+
+    private ILuminaRepository iLuminaRepository;
+
+    public LuminaService(ILuminaRepository iLuminaRepository) {
+        this.iLuminaRepository = iLuminaRepository;
+    }
+
+
 }
