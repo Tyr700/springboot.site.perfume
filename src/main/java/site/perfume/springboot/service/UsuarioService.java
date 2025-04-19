7a73cc8039f63dc867cabc77993b37af1da7345f
Tyr700 <tlolivira970@gmail.com>
2025-04-19 14:51:02 -0300

    Feat: criação dos métodos criar usuario, buscar por email e deletar por email

--- src/main/java/site/perfume/springboot/service/UsuarioService.java
+++ src/main/java/site/perfume/springboot/service/UsuarioService.java
@@ -1,9 +1,38 @@
 package site.perfume.springboot.service;
 
+import java.util.List;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import site.perfume.springboot.errors.UsuarioJaExiste;
 import site.perfume.springboot.model.Usuario;
+import site.perfume.springboot.repository.IUsuarioRepository;
+
 
 @Service
 public class UsuarioService {
+
+    private IUsuarioRepository usuarioRepository;
+
+    public UsuarioService(IUsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario criarUsuario(Usuario userModel) {
+        var userExistente = usuarioRepository.findByEmail(userModel.getEmail());
+        if (userExistente != null) {
+            throw new UsuarioJaExiste();
+        }
+        return userExistente;
+    }
+
+    public Usuario buscarPorEmail (String email) {
+        usuarioRepository.findByEmail(email);
+        return null;
+    }
+
+    public void deletePorEmail (String email) {
+        usuarioRepository.deleteByEmail(email);
+    }
+
 }
