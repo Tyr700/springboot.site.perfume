039d8914588e43e93a9dd85b87550399140022a8
Tyr700 <tlolivira970@gmail.com>
2025-04-19 23:23:17 -0300

    Feat: feito o básico de LuminaController e LuminaService

--- src/main/java/site/perfume/springboot/controller/LuminaController.java
+++ src/main/java/site/perfume/springboot/controller/LuminaController.java
@@ -3,10 +3,15 @@ package site.perfume.springboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.perfume.springboot.service.LuminaService;
 
 @RestController
 @RequestMapping("/Lumina")
 public class LuminaController {
 
+    private LuminaService luminaService;
 
+    public LuminaController(LuminaService luminaService) {
+        this.luminaService = luminaService;
+    }
 }
--- src/main/java/site/perfume/springboot/service/LuminaService.java
+++ src/main/java/site/perfume/springboot/service/LuminaService.java
@@ -2,12 +2,15 @@ package site.perfume.springboot.service;
 
 
 import org.springframework.stereotype.Service;
+import site.perfume.springboot.model.Usuario;
 import site.perfume.springboot.repository.ILuminaRepository;
+
+import java.util.List;
 
 @Service
 public class LuminaService {
 
-    private ILuminaRepository iLuminaRepository;
+    private final ILuminaRepository iLuminaRepository;
 
     public LuminaService(ILuminaRepository iLuminaRepository) {
         this.iLuminaRepository = iLuminaRepository;
