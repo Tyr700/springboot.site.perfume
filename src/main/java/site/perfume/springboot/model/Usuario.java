9df98318144976b30f026e1e79ba69ec611e1a85
Tyr700 <tlolivira970@gmail.com>
2025-04-19 23:25:35 -0300

    Feat: mudança de column em Usuario e criação de métodos básicos em ConsultorService

--- src/main/java/site/perfume/springboot/model/Usuario.java
+++ src/main/java/site/perfume/springboot/model/Usuario.java
@@ -5,24 +5,19 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 
-@Setter
-@Getter
+@Data
 @Entity(name = "Usuario")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Usuario {
 
     @Id
-    @Column(name = "id")
     @GeneratedValue(generator = "UUID")
     private UUID id;
 
@@ -32,7 +27,7 @@ public class Usuario {
 
     @NotBlank
     @Email
-    @Column(name = "email_usuario")
+    @Column(name = "email_usuario", unique = true)
     private String email;
 
     @NotBlank
--- src/main/java/site/perfume/springboot/service/ConsultorService.java
+++ src/main/java/site/perfume/springboot/service/ConsultorService.java
@@ -1,4 +1,34 @@
 package site.perfume.springboot.service;
 
+
+import org.springframework.stereotype.Service;
+import site.perfume.springboot.errors.ConsultorJaExiste;
+import site.perfume.springboot.model.Consultor;
+import site.perfume.springboot.model.Usuario;
+import site.perfume.springboot.repository.IConsultorRepository;
+
+import java.util.List;
+
+@Service
 public class ConsultorService {
+
+    private final IConsultorRepository consultorRepository;
+
+    public ConsultorService(IConsultorRepository consultorRepository) {
+        this.consultorRepository = consultorRepository;
+    }
+
+
+    public Consultor criarConsultor(Consultor consultorModel) {
+        var consultorExistente = consultorRepository.findByDocumento(consultorModel.getDocumento());
+        if (consultorExistente != null) {
+            throw new ConsultorJaExiste();
+        }
+        return null;
+    }
+
+    public List<Consultor> listarConsultores() {
+        return consultorRepository.findAll();
+    }
+
 }
